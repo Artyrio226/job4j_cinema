91b583e4c4888d2b3e1df33af3d278e9cdfbf398
Artyrio226 <artur_sar_80@mail.ru>
2024-02-13 12:43:18 +0300

    2. Сервис - Кинотеатр [#504869] добавлены контроллеры

---
+++ src/main/java/ru/job4j/cinema/controller/SessionController.java
@@ -0,0 +1,40 @@
+package ru.job4j.cinema.controller;
+
+import net.jcip.annotations.ThreadSafe;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.job4j.cinema.service.SessionService;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ThreadSafe
+@RequestMapping("/sessions")
+@Controller
+public class SessionController {
+    private final SessionService sessionService;
+
+    public SessionController(SessionService sessionService) {
+        this.sessionService = sessionService;
+    }
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("filmSessions", sessionService.findAll());
+        return "sessions/list";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(Model model, @PathVariable int id, HttpServletRequest request) {
+        var sessionOptional = sessionService.findById(id);
+        if (sessionOptional.isEmpty()) {
+            model.addAttribute("message", "Сеанс с указанным идентификатором не найден");
+            return "errors/404";
+        }
+        model.addAttribute("filmSession", sessionOptional.get());
+        request.getSession().setAttribute("sessionDto", sessionOptional.get());
+        return "sessions/one";
+    }
+}
---
+++ src/main/java/ru/job4j/cinema/controller/TicketController.java
@@ -0,0 +1,45 @@
+package ru.job4j.cinema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.job4j.cinema.dto.SessionDto;
+import ru.job4j.cinema.model.Ticket;
+import ru.job4j.cinema.model.User;
+import ru.job4j.cinema.service.TicketService;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/tickets")
+public class TicketController {
+    private final TicketService ticketService;
+
+    public TicketController(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+
+    @PostMapping("/buy")
+    public String register(Model model, HttpSession session,
+                           @ModelAttribute Ticket ticket) {
+        var user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "/users/login";
+        }
+        var sessionDto = (SessionDto) session.getAttribute("sessionDto");
+        var foundTicket = ticketService.findUniqueTicket(sessionDto.getId(), ticket.getRowNumber(), ticket.getPlaceNumber());
+        if (foundTicket.isPresent()) {
+            model.addAttribute("message",
+                    "Не удалось приобрести билет на заданное место. "
+                            + "Вероятно оно уже занято. Перейдите на страницу бронирования билетов "
+                            + "и попробуйте снова.");
+            return "tickets/failure";
+        }
+        var savedTicket = ticketService.save(ticket, sessionDto.getId(), user.getId());
+        model.addAttribute("message",
+                String.format("Вы успешно приобрели билет. Ряд: %s, место: %s",
+                        savedTicket.getRowNumber(),
+                        savedTicket.getPlaceNumber()));
+        return "tickets/success";
+    }
+}
